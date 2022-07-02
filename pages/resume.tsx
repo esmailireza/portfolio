907e67d19f9b97f7bfb476e12a3c75a2b0824568
esmailireza <esmailireza20@gmail.com>
2022-07-02 21:02:50 +0430

    implement resume component

---
+++ pages/resume.tsx
@@ -0,0 +1,55 @@
+import Bar from "../components/Bar";
+import { languages, tools } from "../data";
+
+const Resume = () => {
+  return (
+    <div className="px-6 py-2">
+      {/* //! Education & Experience */}
+      <div className="grid gap-6 md:grid-cols-2">
+        <div>
+          <h5 className="my-3 text-2xl font-bold">Education</h5>
+          <div className="">
+            <h5 className="my-2 text-xl font-bold">
+            Computer Software Engineering
+            </h5>
+            <p className="font-semibold">Islamic Azad University (2016-2018)</p>
+            <p className="my-3">
+              Associate's degree, Computer Software Engineering at (IAU) of Iran/urmia
+            </p>
+          </div>
+        </div>
+        <div>
+          <h5 className="my-3 text-2xl font-bold">Experience</h5>
+          <div className="">
+            <h5 className="my-2 text-xl font-bold">Frontend developer at <a style={{ textDecoration: "none" }} href="https://tadbirnegar.ir/">Tadbirnegar</a> </h5>
+            <p className="font-semibold">Full Time - Sep 2020 - Nov 2021</p>
+            <p className="my-3">Frontend developer at Tadbirnegar</p>
+          </div>
+        </div>
+      </div>
+
+      {/*Languages & Tools */}
+      <div className="grid gap-9 md:grid-cols-2">
+        <div>
+          <h5 className="my-3 text-2xl font-bold">Language & Framework</h5>
+          <div className="my-2 ">
+            {languages.map((language, i) => (
+              <Bar value={language} key={i} />
+            ))}
+          </div>
+        </div>
+
+        <div>
+          <h5 className="my-3 text-2xl font-bold">&nbsp;</h5>
+          <div className="my-2">
+            {tools.map((tool, i) => (
+              <Bar value={tool} key={i} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Resume;
